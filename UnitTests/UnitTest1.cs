97f5e8fa51ce4d23e50b01d3e2a10959529beb2a
Arewic <arevik.antonyan.1998@mail.ru>
2019-11-20 20:30:31 +0300

    Algorithm changed

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordSearch;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        char[,] board = new char[,] { { 'A', 'E', 'D' },
+                                      { 'D', 'J', 'B' },
+                                      { 'А', 'B', 'C' } };
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(Program.Exist(board, "AED"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(Program.Exist(board, "AEJ"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsFalse(Program.Exist(board, "DJA"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.IsTrue(Program.Exist(board, "CB"));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.IsFalse(Program.Exist(board, "CBC"));
+        }
+    }
+}
--- WordSearch/Program.cs
+++ WordSearch/Program.cs
@@ -6,10 +6,72 @@ using System.Threading.Tasks;
 
 namespace WordSearch
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            char[,] board = new char[,] { { 'A', 'E', 'D' },
+                                          { 'D', 'J', 'B' },
+                                          { 'А', 'B', 'C' } };
+            Console.WriteLine(Exist(board, "DJA"));
+            Console.ReadKey();
+        }
+        //Число соседних ячеек 
+        static int GetNumberNeighbors(char[,] table, int line, int col)
+        {
+            int n = table.GetLength(0);
+            int m = table.GetLength(1);
+            int num = 4;
+            if ((line == 0 || line == n - 1) && (col == 0 || col == m - 1))
+                num = 2;
+            else if ((line == 0 || line == n - 1) || (col == 0 || col == m - 1))
+                num = 3;
+            return num;
+        }
+        public static bool Exist(char[,] board, string word)
+        {
+            if (word.Length == 0)
+                return false;
+            int n = board.GetLength(0);//кол-во строк 
+            int m = board.GetLength(1);//кол-во столбцов
+            int[] look = new int[n * m];//просмотренные буквенные ячейки
+            char[] w = word.ToCharArray();
+            int br, l, c, x;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (board[i, j] == w[0])
+                    {
+                        br = GetNumberNeighbors(board,i,j);
+                        Array.Clear(look, 0, n * m);
+                        l = i; c = j; x = 0; 
+                        while (true)
+                        {
+                            x++;
+                            if (x == word.Length)
+                                return true;
+                            look[l * n + c] = 1;
+                            if (c - 1 >= 0 && board[l, c - 1] == w[x] && look[l * n + c - 1] != 1)
+                                c--;
+                            else if (l - 1 >= 0 && board[l - 1, c] == w[x] && look[(l - 1) * n + c] != 1)
+                                l--;
+                            else if (c + 1 < m && board[l, c + 1] == w[x] && look[l * n + c + 1] != 1)
+                                c++;
+                            else if (l + 1 < n && board[l + 1, c] == w[x] && look[(l + 1) * n + c] != 1)
+                                l++;
+                            else if (br == 0)
+                                break;
+                            else
+                            {
+                                Array.Clear(look, 0, n * m);
+                                l = i; c = j; x = 0; br--;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
         }
     }
 }
